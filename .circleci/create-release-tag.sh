aff30fa328718db6ee767b37f73146b6d18e3735
_MoonLight_ <minhngoc@MinhNgocs-MacBook-Air.local>
2019-12-22 00:26:05 +0700

    ignore master, fix script release

---
+++ .circleci/create-release-tag.sh
@@ -0,0 +1,24 @@
+TIMESTAMP=$1
+VERSION=$2
+BRANCH=$3
+COMMIT_ID=$4
+echo "$TIMESTAMP $VERSION $BRANCH $COMMIT_ID"
+
+http_code=$(curl --silent --output release.json --write-out '%{http_code}' \
+  --data '{
+    "tag_name": "release-$TIMESTAMP",
+    "target_commitish": "$BRANCH",
+    "name": "Version $VERSION",
+    "body": "- Commit ID: $COMMIT_ID\r\n- Released time: $TIMESTAMP",
+    "draft": false,
+    "prerelease": false
+  }' \
+  --header "Authorization: token $GITHUB_ACCESS_TOKEN" \
+  https://api.github.com/repos/moonlight8978/moonlight8978.github.io/releases)
+
+if [ $http_code = 201 ]; then
+  exit 0
+else
+  cat release.json
+  exit 1
+fi
