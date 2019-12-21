8b03170e3f37e066530d61d0258ece77478cb6a3
_MoonLight_ <minhngoc@MinhNgocs-MacBook-Air.local>
2019-12-22 01:07:46 +0700

    split check, fix deploy

--- .circleci/create-release-tag.sh
+++ .circleci/create-release-tag.sh
@@ -4,17 +4,19 @@ BRANCH=$3
 COMMIT_ID=$4
 echo "$TIMESTAMP $VERSION $BRANCH $COMMIT_ID"
 
-http_code=$(curl --silent --output release.json --write-out '%{http_code}' \
+curl_command="curl --silent --output release.json --write-out '%{http_code}' \
   --data '{
-    "tag_name": "release-$(TIMESTAMP)",
-    "target_commitish": "$(BRANCH)",
-    "name": "Version $(VERSION)",
-    "body": "- Commit ID: $(COMMIT_ID)\r\n- Released time: $(TIMESTAMP)",
-    "draft": false,
-    "prerelease": false
-  }' \
-  --header "Authorization: token $GITHUB_ACCESS_TOKEN" \
-  https://api.github.com/repos/moonlight8978/moonlight8978.github.io/releases)
+    \"tag_name\": \"release-$TIMESTAMP\",
+    \"target_commitish\": \"$BRANCH\",
+    \"name\": \"Version $VERSION\",
+    \"body\": \"- Commit ID: $COMMIT_ID\r\n- Released time: $TIMESTAMP\",
+    \"draft\": false,
+    \"prerelease\": false
+    }' \
+  --header \"Authorization: token $GITHUB_ACCESS_TOKEN\" \
+  --header \"Content-Type: application/json\" \
+  https://api.github.com/repos/moonlight8978/moonlight8978.github.io/releases"
+http_code=$(eval $curl_command)
 
 if [ $http_code = 201 ]; then
   exit 0
