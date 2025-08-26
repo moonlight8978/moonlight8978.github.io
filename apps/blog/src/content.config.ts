e0aae7465040877fdf302288387d0fa96b44c3b2
_MoonLight_ <moonlight8978@gmail.com>
2025-08-27 00:55:45 +0700

    feat: short introduction

--- apps/blog/src/content.config.ts
+++ apps/blog/src/content.config.ts
@@ -1,19 +1,33 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
 });
 
-export const collections = { blog };
+const home = defineCollection({
+  loader: glob({ pattern: ["home.md", "home.mdx"], base: "./src/content" }),
+  schema: ({ image }) =>
+    z.object({
+      avatarImage: z
+        .object({
+          src: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      githubCalendar: z.string().optional(), // GitHub username for calendar
+    }),
+});
+
+export const collections = { blog, home };
---
+++ apps/blog/src/content/home.md
@@ -0,0 +1,10 @@
+---
+avatarImage:
+  src: ./avatar.jpg
+  alt: moonlight8978
+githubCalendar: moonlight8978
+---
+
+Welcome to my blog! I'm a backend developer skilled in Node.js (TypeScript), Golang, and Ruby, with a strong focus on AWS and Kubernetes as a DevOps engineer.
+<br />
+Here, I share insights, tutorials, and experiences from my journey in software development and cloud engineering. Enjoy reading!
